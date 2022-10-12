1ee23cd9c6e0c59b49d20398e78b0ea04980ef31
memento <vpmemento@gmail.com>
2022-10-12 11:59:31 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
-// import './App.css';
-// import {useEffect} from "react";
-// import {useTelegram} from "./hooks/useTelegram";
-// import Header from "./components/Header/Header"
-// import {Route, Routes} from "react-router-dom";
-// import ProductList from "./components/ProductList/ProductList";
-// import Form from "./components/Form/Form";
-//
-// function App() {
-//
-//     const {tg} = useTelegram()
-//
-//     useEffect(() => {
-//         tg.ready()
-//     }, [])
-//
-//
-//   return (
-//     <div className="App">
-//         <Header />
-//
-//         <Routes>
-//             <Route index element = {<ProductList />} />
-//             <Route path = {'form'} element = {<Form />} />
-//         </Routes>
-//
-//     </div>
-//   );
-// }
-//
-// export default App;
-
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
+import Header from "./components/Header/Header"
+import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+
+    const {tg} = useTelegram()
 
     useEffect(() => {
-        tg.ready();
+        tg.ready()
     }, [])
 
-    return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
-            </Routes>
-        </div>
-    );
+
+  return (
+    <div className="App">
+        <Header />
+
+        <Routes>
+            <Route index element = {<ProductList />} />
+            <Route path = {'form'} element = {<Form />} />
+        </Routes>
+
+    </div>
+  );
 }
 
 export default App;
+
