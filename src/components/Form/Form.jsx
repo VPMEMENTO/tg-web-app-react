305e09da9c39cfc05eed4a01dc60cd5a2a0d04a4
memento <vpmemento@gmail.com>
2022-10-13 07:52:30 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -11,23 +11,23 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram()
 
-    const onSendData = useCallback(() => {
-        const data = {
-            country,
-            street,
-            subject
-        }
-        tg.sendData(JSON.stringify(data))
+    // const onSendData = useCallback(() => {
+    //     const data = {
+    //         country,
+    //         street,
+    //         subject
+    //     }
+    //     tg.sendData(JSON.stringify(data))
+    //
+    // }, [])
 
-    }, [])
-
-    useEffect(() => {
-        tg.WebApp.onEvent('mainButtonClicked', onSendData())
-        return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData())
-
-        }
-    }, [])
+    // useEffect(() => {
+    //     tg.WebApp.onEvent('mainButtonClicked', onSendData())
+    //     return () => {
+    //         tg.WebApp.offEvent('mainButtonClicked', onSendData())
+    //
+    //     }
+    // }, [])
 
     useEffect(() => {
         tg.MainButton.setParams({
