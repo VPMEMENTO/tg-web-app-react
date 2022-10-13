4d53dcca08ef52a736a5ebb556a6bae168b9d3f5
memento <vpmemento@gmail.com>
2022-10-13 07:54:38 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -29,6 +29,23 @@ const Form = () => {
     //     }
     // }, [])
 
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
