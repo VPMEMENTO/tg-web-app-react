30d53b809c2bce7048ff9bc41fb22ad5b34c3e0c
memento <vpmemento@gmail.com>
2022-10-15 11:01:57 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -15,71 +15,16 @@ const products = [
     {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-// const getTotalPrice = (items = []) => {
-//     return items.reduce((acc, item) => {
-//         return acc += item.price
-//     }, 0)
-// }
-//
-// //Корзина
-// const ProductList = () => {
-//     const [addedItems, setAddedItems] = useState([]);
-//     const {tg} = useTelegram()
-//     const onAdd = (product) => {
-//         const alreadyAdded = addedItems.find(item => item.id === product.id);
-//         let newItems = [];
-//
-//         if(alreadyAdded) {
-//             newItems = addedItems.filter(item => item.id !== product.id);
-//         } else {
-//             newItems = [...addedItems, product];
-//         }
-//
-//         setAddedItems(newItems)
-//
-//         if (newItems.length === 0) {
-//             tg.MainButton.hide();
-//         } else {
-//             tg.MainButton.show();
-//             tg.MainButton.setParams({
-//                 text: `Купить ${getTotalPrice(newItems)}`
-//             })
-//         }
-//     }
-
-
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
     }, 0)
 }
 
+//Корзина
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
-
-    const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
-        fetch('http://85.119.146.179:8000/web-data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems])
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
-
+    const {tg} = useTelegram()
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
@@ -92,7 +37,7 @@ const ProductList = () => {
 
         setAddedItems(newItems)
 
-        if(newItems.length === 0) {
+        if (newItems.length === 0) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
@@ -101,7 +46,6 @@ const ProductList = () => {
             })
         }
     }
-
 
     return (
         <div className={'list'}>
