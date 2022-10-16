f0bc730e30d1281efe0a5b5a9446da1541249e80
memento <vpmemento@gmail.com>
2022-10-16 09:24:29 +0200

    init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,12 @@
-// import React from 'react';
-// import './Button.css';
-//
-// const Button = (props) => {
-//     return (
-//         <button {...props} className = {'button ' +  props.className}/>
-//
-//
-//
-//     );
-// };
-//
-// export default Button;
-
 import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
     return (
-        <button {...props} className={'button ' + props.className}/>
+        <button {...props} className = {'button ' +  props.className}/>
+
     );
 };
 
 export default Button;
+
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
+import Button from "../Button/Button";
 import './Header.css';
 
 const Header = () => {
@@ -7,7 +8,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <button onClick={onClose}>Закрыть</button>
+            <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
