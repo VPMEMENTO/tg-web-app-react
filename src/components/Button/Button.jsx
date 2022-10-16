b8a383d40c5bf28e9fe3cac172be236c6a815110
memento <vpmemento@gmail.com>
2022-10-16 09:09:02 +0200

    init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,23 @@
+// import React from 'react';
+// import './Button.css';
+//
+// const Button = (props) => {
+//     return (
+//         <button {...props} className = {'button ' +  props.className}/>
+//
+//
+//
+//     );
+// };
+//
+// export default Button;
+
 import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
     return (
-        <button {...props} className = {'button ' +  props.className}/>
-
-
+        <button {...props} className={'button ' + props.className}/>
     );
 };
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import "../Form/Form.css";
+import "./Form.css";
 import {useTelegram} from "../../hooks/useTelegram";
 
 
